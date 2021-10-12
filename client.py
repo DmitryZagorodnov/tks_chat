951666deed29215f54817c69d3e44fa0ba9ace3a
DmZ <zagorodnov.dimka@gmail.com>
2021-10-13 01:29:04 +0300

    added files

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import socket
 import threading
-import os
-import sys
 
 FORMAT = "utf-8"
 HEADER = 64
 PORT = 5050
 DISCONNECT_COMMAND = "!DISCONNECT"
-SERVER = "192.168.0.5"
+ADD_FILE_COMMAND = "!ATTACH"
+GET_FILE_COMMAND = "!FILE COMES NEXT"
+SERVER = "172.29.192.1"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,41 +15,83 @@ client.connect(ADDR)
 connected = True
 
 
+"""
+Gets messages from the server. If message started with GET_FILE_COMMAND it is getting file's size and waiting for the
+file to write 
+"""
+
+
 def get_message(conn):
     while True:
-        msg_length = conn.recv(HEADER).decode(FORMAT)  # Waits til some message will come throw the socket
-        if not msg_length:
-            print('')
-        else:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            print(msg)
+        try:
+            msg_length = conn.recv(HEADER).decode(FORMAT)  # Waits til some message will come throw the socket
+            if not msg_length:
+                break
+            else:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg.startswith(GET_FILE_COMMAND):
+                    name = msg.split()[3]
+                    image_length = conn.recv(HEADER).decode(FORMAT)
+                    image_length = int(image_length)
+                    image = conn.recv(image_length)
+                    with open(name, 'wb') as file:
+                        file.write(image)
+                    print(f"You got the file named {name}")
+                else:
+                    print(msg)
+        except ConnectionAbortedError:
+            print("Rerun the executable file")
+
+
+"""
+Converts the image to the string of bytes
+"""
+
+
+def attach_file(path):
+    with open(path, 'rb') as image:
+        file = image.read()
+        return file
+
+
+"""
+Sends messages to the server. If it is ADD_FILE_COMMAND sends service message and file to the server
+"""
 
 
 def send():
     while True:
-        msg = input()
-        message = msg.encode(FORMAT)
-        msg_length = len(message)
-        send_length = str(msg_length).encode(FORMAT)
-        send_length += b' ' * (HEADER - len(send_length))
-        client.send(send_length)
-        client.send(message)
+        try:
+            msg = input()
+            message = msg.encode(FORMAT)
+            msg_length = len(message)
+            send_length = str(msg_length).encode(FORMAT)
+            send_length += b' ' * (HEADER - len(send_length))
+            client.send(send_length)
+            client.send(message)
+            if msg.startswith(ADD_FILE_COMMAND):
+                path = msg.split()[1]
+                image = attach_file(path)
+                image_length = len(image)
+                send_image_length = str(image_length).encode(FORMAT)
+                send_image_length += b' ' * (HEADER - len(send_image_length))
+                client.send(send_image_length)
+                client.send(image)
+        except ConnectionAbortedError:
+            print("Rerun the executable file")
 
 
-print("Write !LOGIN [name] to login the server\n")
-'''send()'''
-'''get_message(client)'''
+"""
+Starting the threads to get and to send messages
+"""
+
+print("Write !LOGIN [name] to login the server")
 
 while True:
-    try:
-        getting_thread = threading.Thread(target=get_message, args=(client, ))
-        sending_thread = threading.Thread(target=send)
-        getting_thread.start()
-        sending_thread.start()
-        getting_thread.join()
-        sending_thread.join()
-    except ConnectionAbortedError:
-        print("You haven't logged to the server. Rerun the executable file")
-    '''except ValueError:
-        pass'''
+    getting_thread = threading.Thread(target=get_message, args=(client, ))
+    sending_thread = threading.Thread(target=send)
+    getting_thread.start()
+    sending_thread.start()
+    getting_thread.join()
+    sending_thread.join()
--- server.py
+++ server.py
@@ -9,75 +9,37 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 DISCONNECT_COMMAND = "!DISCONNECT"
 LOGIN_COMMAND = "!LOGIN"
+ADD_FILE_COMMAND = "!ATTACH"
+SEND_FILE_COMMAND = "!FILE COMES NEXT"
 
 clients_list = {}
-
-print(SERVER)
+clients_online = []
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 mutex = threading.Lock()
 
-
-def send_message(conn, msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    conn.send(send_length)
-    conn.send(message)
-
-
-def handle_client(conn, addr):
-    global connected
-    print("in hc")
-    connected = True
-
-    if addr not in clients_list:
-        login_message = get_message(conn)
-        if login_message.startswith(LOGIN_COMMAND):
-            clients_list[addr] = [login_message.split()[1], 'connected', conn]
-        else:
-            connected = False
-    else:
-        clients_list[addr][1] = 'connected'
-
-    print("b4 if conn")
-
-    if connected:
-        print(f"\nNEW CONNECTION: {clients_list[addr][0]} connected", end='\n')
-        print(f"THE NUMBER OF CURRENT CONNECTIONS {threading.activeCount() - 1}")
-        send_message(conn, "You are logged in")
-        print(clients_list)
-
-    while connected:
-        message = get_message(conn)
-        if message == -1:
-            connected = False
-            break
-        if message:
-            ans = f"<{time.asctime()}> [{clients_list[addr][0]}] {message}"
-            print(ans)
-            mutex.acquire()
-            for addr in clients_list:
-                if clients_list[addr][1] == 'connected':
-                    send_message(clients_list[addr][2], ans)
-            mutex.release()
-        connected = clients_list[addr][1]
-    conn.close()
+"""
+This function is needed to remove the disconnected users from the server. Also notifies them of disconnection 
+"""
 
 
 def disconnect(conn):
-    for addr in clients_list:
-        if clients_list[addr][2] == conn:
-            clients_list[addr][1] = 'disconnected'
-            print(f"[{clients_list[addr][0]}] DISCONNECTED FROM THE SERVER")
-            '''threading.current_thread().join()'''
+    for login in clients_list:
+        if clients_list[login] == conn:
+            print(f"[{login}] DISCONNECTED FROM THE SERVER")
+            send_message(conn, "YOU ARE DISCONNECTED FROM THE SERVER")
+            clients_online.remove(login)
+
+
+"""
+Gets a messages from the client's socket. Firstly gets a length of the message comes next, then it itself 
+"""
 
 
 def get_message(conn):
     try:
-        msg_length = conn.recv(HEADER).decode(FORMAT)  # Waits til some message will come throw the socket
+        msg_length = conn.recv(HEADER).decode(FORMAT)
         if not msg_length:
             return ''
         else:
@@ -85,6 +47,7 @@ def get_message(conn):
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg.startswith(DISCONNECT_COMMAND):
                 disconnect(conn)
+                return -1
             else:
                 return msg
     except ConnectionResetError:
@@ -92,11 +55,104 @@ def get_message(conn):
         return -1
 
 
+"""
+Gets a socket and message to send. Firstly sending the length of the message complemented to the HEADER's size to the
+client's socket, secondly sending the message. If it is not possible to send disconnects current client from the server
+"""
+
+
+def send_message(conn, msg):
+    try:
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        conn.send(send_length)
+        conn.send(message)
+    except ConnectionResetError:
+        disconnect(conn)
+
+
+"""
+If you need to send the file, call this function. It firstly sends a service message with a name of the file, secondly 
+sending a file size and thirdly the file itself like a string of bytes  
+"""
+
+
+def send_file(conn, name, file):
+    send_message(conn, SEND_FILE_COMMAND + " " + name)
+    file_length = len(file)
+    send_length = str(file_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    conn.send(send_length)
+    conn.send(file)
+
+
+"""
+Main function that receives messages from clients and sends them back. Waits a LOGIN_COMMAND as first message and starts
+to handle client if it is right. When user disconnects closing the connection. 
+"""
+
+
+def handle_client(conn, addr):
+    global connected
+    connected = True
+
+    login_message = get_message(conn)
+    if login_message != -1 and login_message.startswith(LOGIN_COMMAND):
+        login = login_message.split()[1]
+        if login in clients_online:
+            connected = False
+        else:
+            clients_online.append(login)
+            clients_list[login] = conn
+    else:
+        send_message(conn, "YOU ARE NOT LOGGED INTO THE SERVER. RERUN THE EXECUTABLE FILE")
+        connected = False
+
+    if connected:
+        print(f"\nNEW CONNECTION: {login} connected", end='\n')
+        print(f"THE NUMBER OF CURRENT CONNECTIONS {threading.activeCount() - 1}")
+        send_message(conn, "You are logged in")
+    while connected:
+        message = get_message(conn)
+        if message == -1:
+            connected = False
+            break
+        elif message.startswith(ADD_FILE_COMMAND):
+            name = message.split()[1]
+            print(f"<{time.asctime()}> [{login}] is sending a file {name}")
+            image_length = conn.recv(HEADER).decode(FORMAT)
+            image_length = int(image_length)
+            image = conn.recv(image_length)
+            mutex.acquire()
+            for client in clients_online:
+                if client != login:
+                    send_file(clients_list[client], name, image)
+                else:
+                    send_message(clients_list[client], "Your file has been delivered")
+            mutex.release()
+        elif message:
+            ans = f"<{time.asctime()}> [{login}] {message}"
+            print(ans)
+            mutex.acquire()
+            for client in clients_online:
+                send_message(clients_list[client], ans)
+            mutex.release()
+        connected = login in clients_online
+    conn.close()
+
+
+"""
+This function makes the server listening for new connections. When it occurs starting a thread for each new client 
+"""
+
+
 def start():
     server.listen()
     print(f'SERVER IS LISTENING ON {SERVER}')
     while True:
-        conn, addr = server.accept()            # Blocking and waiting for the new connection
+        conn, addr = server.accept()  # Blocking and waiting for the new connection
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
 
