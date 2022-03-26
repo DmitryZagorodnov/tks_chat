23bdd00534bf1ae1c5cea2b817fbc157e59be68c
DmitryZagorodnov <43351927+DmitryZagorodnov@users.noreply.github.com>
2022-03-27 02:23:58 +0300

    delete

--- TEST.py
+++
@@ -1,8 +0,0 @@
-st = "фывффв"
-HEADER = 10
-FORMAT = 'utf-8'
-msg_length = len(st)
-send_length = str(msg_length).encode(FORMAT)
-send_length += b' ' * (HEADER - len(send_length))
-
-print(send_length, len(send_length))
